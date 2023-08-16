2c89026ddacc0b3b0ef82c0f6c30a1c533d73e93
Michky23 <michael23.micky@gmail.com>
2023-08-16 01:01:47 -0700

    the first assignment was to write a program to print the first 100 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
-  * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
-  * less than 4000000.
-  *
-  * Return: Nothing!
-  */
+ * main - prints the first 98 Fibonacci numbers
+ * starting with 1 and 2 less than 4000000.
+ * Return: Nothing!
+ */
+
 int main(void)
+
 {
 	int i = 0;
+
 	long j = 1, b = 2, sum = b;
 
 	while (b + j < 4000000)
