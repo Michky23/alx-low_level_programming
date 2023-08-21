5a460f2aeef26ac08dc4ec14e64229ce231e37c4
Michky23 <michael23.micky@gmail.com>
2023-08-21 09:20:12 -0700

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,6 +3,7 @@
 /**
  * print_rev - function that prints a string, in reverse
  * @s: string to be printed
+ * Return: void
  */
 
 void print_rev(char *s)
@@ -13,7 +14,7 @@ void print_rev(char *s)
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for (i = count - 1 ; i >= 0 ; i++)
+	for (i = count - 1 ; i >= 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
