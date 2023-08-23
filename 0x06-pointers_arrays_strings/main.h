f583a32905487b83b71cda341e9d4a6b0ffe9f42
Michky23 <michael23.micky@gmail.com>
2023-08-23 15:28:03 -0700

    strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -12,5 +13,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 
 #endif
